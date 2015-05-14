1df12d0e7b7c7279bf8be3007e48fe298e9e6e73
Tuncay Altinpulluk <tuncayalt@gmail.com>
2015-05-15 01:32:15 +0300

    editCustomType, getCustomTypeById, createPost,  getPostListByCustomTypeId, getPostById, getAttributeListByCustomTypeId added.

--- TimeOutBackend/TimeoutBackend/src/main/java/demo/CustomTypeRestServices.java
+++ TimeOutBackend/TimeoutBackend/src/main/java/demo/CustomTypeRestServices.java
@@ -26,6 +26,7 @@ import common.ResponseHeader;
 import entity.Action;
 import entity.Attribute;
 import entity.CustomType;
+import entity.Post;
 import entity.User;
 
 @RestController
@@ -48,6 +49,58 @@ public class CustomTypeRestServices {
 			CustomTypeRepository cr = new CustomTypeRepository(em);
 			
 			customType = cr.createCustomType(user, actionId, name, attributesJsonListString);
+			DBUtility.commitTransaction(em);
+		} catch (BusinessException e) {
+			DBUtility.rollbackTransaction(em);
+			return new ResponseHeader(false, e.getCode(), e.getMessage());
+		} catch (Exception e) {
+			DBUtility.rollbackTransaction(em);
+			return new ResponseHeader(false, e.getMessage());
+		}
+		return customType;
+	}
+	
+	@RequestMapping(value = "/customType/edit")
+	@ResponseBody
+	public Object editCustomType(
+            @RequestParam(value = "sessionId") String sessionId, 
+            @RequestParam(value = "customTypeId") Long customTypeId,
+            @RequestParam(value = "name") String name,
+            HttpServletResponse resp) {
+		EntityManager em = ServiceHelper.initialize(resp);
+		
+		CustomType customType;
+		try {
+			User user = ServiceHelper.getSessionUser(em, sessionId);
+			
+			CustomTypeRepository cr = new CustomTypeRepository(em);
+			
+			customType = cr.editCustomType(customTypeId, name);
+			DBUtility.commitTransaction(em);
+		} catch (BusinessException e) {
+			DBUtility.rollbackTransaction(em);
+			return new ResponseHeader(false, e.getCode(), e.getMessage());
+		} catch (Exception e) {
+			DBUtility.rollbackTransaction(em);
+			return new ResponseHeader(false, e.getMessage());
+		}
+		return customType;
+	}
+	
+	@RequestMapping(value = "/customType/getById")
+	@ResponseBody
+	public Object getCustomTypeById(
+            @RequestParam(value = "sessionId") String sessionId, 
+            @RequestParam(value = "customTypeId") Long customTypeId,
+            HttpServletResponse resp) {
+		EntityManager em = ServiceHelper.initialize(resp);
+		
+		CustomType customType;
+		try {
+			User user = ServiceHelper.getSessionUser(em, sessionId);
+			
+			CustomTypeRepository cr = new CustomTypeRepository(em);
+			customType = cr.getCustomTypeById(customTypeId);
 			DBUtility.commitTransaction(em);
 		} catch (BusinessException e) {
 			DBUtility.rollbackTransaction(em);
@@ -83,6 +136,108 @@ public class CustomTypeRestServices {
 		}
 		return customTypes;
 	}
-
-
+	
+	@RequestMapping(value = "/post/create")
+	@ResponseBody
+	public Object createPost(
+            @RequestParam(value = "sessionId") String sessionId, 
+            @RequestParam(value = "customTypeId") Long customTypeId,
+            @RequestParam(value = "title") String title,
+            @RequestParam(value = "text", required = false) String text,
+            @RequestParam(value = "attributeValues", required = false) String attributeValuesJsonListString,
+            HttpServletResponse resp) {
+		EntityManager em = ServiceHelper.initialize(resp);
+		
+		Post post;
+		try {
+			User user = ServiceHelper.getSessionUser(em, sessionId);
+			
+			CustomTypeRepository cr = new CustomTypeRepository(em);
+			
+			post = cr.createPost(user, customTypeId, title, text, attributeValuesJsonListString);
+			DBUtility.commitTransaction(em);
+		} catch (BusinessException e) {
+			DBUtility.rollbackTransaction(em);
+			return new ResponseHeader(false, e.getCode(), e.getMessage());
+		} catch (Exception e) {
+			DBUtility.rollbackTransaction(em);
+			return new ResponseHeader(false, e.getMessage());
+		}
+		return post;
+	}
+	
+	@RequestMapping(value = "/post/getListByCustomTypeId")
+	@ResponseBody
+	public Object getPostListByCustomTypeId(
+            @RequestParam(value = "sessionId") String sessionId, 
+            @RequestParam(value = "customTypeId") Long customTypeId,
+            HttpServletResponse resp) {
+		EntityManager em = ServiceHelper.initialize(resp);
+		
+		List<Post> posts;
+		try {
+			User user = ServiceHelper.getSessionUser(em, sessionId);
+			
+			CustomTypeRepository cr = new CustomTypeRepository(em);
+			posts = cr.getPostListByCustomTypeId(customTypeId);
+			DBUtility.commitTransaction(em);
+		} catch (BusinessException e) {
+			DBUtility.rollbackTransaction(em);
+			return new ResponseHeader(false, e.getCode(), e.getMessage());
+		} catch (Exception e) {
+			DBUtility.rollbackTransaction(em);
+			return new ResponseHeader(false, e.getMessage());
+		}
+		return posts;
+	}
+	
+	@RequestMapping(value = "/post/getById")
+	@ResponseBody
+	public Object getPostById(
+            @RequestParam(value = "sessionId") String sessionId, 
+            @RequestParam(value = "postId") Long postId,
+            HttpServletResponse resp) {
+		EntityManager em = ServiceHelper.initialize(resp);
+		
+		Post post;
+		try {
+			User user = ServiceHelper.getSessionUser(em, sessionId);
+			
+			CustomTypeRepository cr = new CustomTypeRepository(em);
+			post = cr.getPostById(postId);
+			DBUtility.commitTransaction(em);
+		} catch (BusinessException e) {
+			DBUtility.rollbackTransaction(em);
+			return new ResponseHeader(false, e.getCode(), e.getMessage());
+		} catch (Exception e) {
+			DBUtility.rollbackTransaction(em);
+			return new ResponseHeader(false, e.getMessage());
+		}
+		return post;
+	}
+	
+	@RequestMapping(value = "/attribute/getListByCustomTypeId")
+	@ResponseBody
+	public Object getAttributeListByCustomTypeId(
+            @RequestParam(value = "sessionId") String sessionId, 
+            @RequestParam(value = "customTypeId") Long customTypeId,
+            HttpServletResponse resp) {
+		EntityManager em = ServiceHelper.initialize(resp);
+		
+		List<Attribute> attributes;
+		try {
+			User user = ServiceHelper.getSessionUser(em, sessionId);
+			
+			CustomTypeRepository cr = new CustomTypeRepository(em);
+			attributes = cr.getAttributeListByCustomTypeId(customTypeId);
+			DBUtility.commitTransaction(em);
+		} catch (BusinessException e) {
+			DBUtility.rollbackTransaction(em);
+			return new ResponseHeader(false, e.getCode(), e.getMessage());
+		} catch (Exception e) {
+			DBUtility.rollbackTransaction(em);
+			return new ResponseHeader(false, e.getMessage());
+		}
+		return attributes;
+	}
 }
--- TimeOutBackend/TimeoutBackend/src/main/java/repository/CustomTypeRepository.java
+++ TimeOutBackend/TimeoutBackend/src/main/java/repository/CustomTypeRepository.java
@@ -2,6 +2,7 @@ package repository;
 
 import java.lang.reflect.Type;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 import javax.persistence.EntityManager;
@@ -14,6 +15,7 @@ import common.DBUtility;
 import entity.Action;
 import entity.Attribute;
 import entity.CustomType;
+import entity.Post;
 import entity.User;
 
 public class CustomTypeRepository {
@@ -66,12 +68,79 @@ public class CustomTypeRepository {
 	public List<CustomType> getCustomTypeListByActionId(Long actionId) {
 		List<CustomType> returnVal = null;
 		if (actionId > 0) {
-			Query query = null;
-			query = em.createQuery(
+			Query query = em.createQuery(
 					"FROM CustomType C WHERE C.actionId = :actionId")
 					.setParameter("actionId", actionId);
 			returnVal = query.getResultList();
 		}
 		return returnVal;
+	}
+
+	public List<Post> getPostListByCustomTypeId(Long customTypeId) {
+		List<Post> returnVal = null;
+		if (customTypeId > 0) {
+			Query query = em.createQuery(
+					"FROM Post P WHERE P.customTypeId = :customTypeId")
+					.setParameter("customTypeId", customTypeId);
+			returnVal = query.getResultList();
+		}
+		return returnVal;
+	}
+
+	public List<Attribute> getAttributeListByCustomTypeId(Long customTypeId) {
+		List<Attribute> returnVal = null;
+		if (customTypeId > 0) {
+			Query query = em.createQuery(
+					"FROM Attribute A WHERE A.customTypeId = :customTypeId")
+					.setParameter("customTypeId", customTypeId);
+			returnVal = query.getResultList();
+		}
+		return returnVal;
+	}
+
+	public CustomType getCustomTypeById(Long customTypeId) {
+		CustomType returnVal = null;
+		if (customTypeId > 0) {
+			Query query = em.createQuery(
+					"FROM CustomType C WHERE C.customTypeId = :customTypeId")
+					.setParameter("customTypeId", customTypeId);
+			returnVal = (CustomType) query.getSingleResult();
+		}
+		return returnVal;
+	}
+
+	public CustomType editCustomType(Long customTypeId, String name) {
+		CustomType customType = null;
+		if (customTypeId > 0) {
+			customType = getCustomTypeById(customTypeId);
+			customType.setName(name);
+			em.persist(customType);
+		}
+		return customType;
+	}
+
+	public Post getPostById(Long postId) {
+		Post returnVal = null;
+		if (postId > 0) {
+			Query query = em.createQuery(
+					"FROM Post C WHERE C.postId = :postId")
+					.setParameter("postId", postId);
+			returnVal = (Post) query.getSingleResult();
+		}
+		return returnVal;
+	}
+
+	public Post createPost(User user, Long customTypeId, String title,
+			String text, String attributeValuesJsonListString) {
+		Post post = null;
+		if (user != null && customTypeId > 0 && !title.isEmpty()){
+			post = new Post();
+			post.setCustomType(getCustomTypeById(customTypeId));
+			post.setTitle(title);
+			post.setText(text);
+			post.setTime(new Date());
+			em.persist(post);
+		}
+		return post;
 	}
 }
