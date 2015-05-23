9ce8bf0fa96f563df42080715afd36288daae92c
Tuncay Altinpulluk <tuncayalt@gmail.com>
2015-05-23 12:32:54 +0300

    userid increment fix

--- TimeOutBackend/TimeoutBackend/src/main/java/entity/User.java
+++ TimeOutBackend/TimeoutBackend/src/main/java/entity/User.java
@@ -1,21 +1,35 @@
 package entity;
 
-import org.hibernate.annotations.GenericGenerator;
-
-import com.fasterxml.jackson.annotation.JsonIgnore;
-
-import javax.persistence.*;
-
 import java.util.Date;
 import java.util.HashSet;
 import java.util.Set;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
+import javax.persistence.PrimaryKeyJoinColumn;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+import org.hibernate.annotations.GenericGenerator;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 @Entity
 @Table(name = "User")
 public class User {
 
 	@Id
-	@GeneratedValue()
+	@GeneratedValue(generator = "incrementUser")
+	@GenericGenerator(name = "incrementUser", strategy = "increment")
 	private Long userId;
 
 	@Column(name = "userName")
