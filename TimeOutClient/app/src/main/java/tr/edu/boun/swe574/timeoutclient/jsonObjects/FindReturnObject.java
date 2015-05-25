9da6e3194566aab852e8ddfb237e8ae0e1c7b219
Ali Haluk Seven <a.halukseven@gmail.com>
2015-05-25 17:45:45 +0300

    friends tab, groups tab. create group function.

---
+++ TimeOutClient/app/src/main/java/tr/edu/boun/swe574/timeoutclient/jsonObjects/FindReturnObject.java
@@ -0,0 +1,7 @@
+package tr.edu.boun.swe574.timeoutclient.jsonObjects;
+
+/**
+ * Created by haluks on 25/05/15.
+ */
+public class FindReturnObject {
+}
---
+++ TimeOutClient/app/src/main/java/tr/edu/boun/swe574/timeoutclient/jsonObjects/Tag.java
@@ -0,0 +1,7 @@
+package tr.edu.boun.swe574.timeoutclient.jsonObjects;
+
+/**
+ * Created by haluks on 23/05/15.
+ */
+public class Tag {
+}
